7065f5f9a16fad1f1676cba27a89f34374ded3a6
vasker <yarinvasket@gmail.com>
2022-03-01 23:25:49 +0200

    more elegant functions for ToNum

--- src/ToNum.hpp
+++ src/ToNum.hpp
@@ -2,10 +2,9 @@
 
 #include <array>
 #include "Cell.hpp"
+#include "TicTacToe.hpp"
 
 namespace ToNum {
-	unsigned int intpow(unsigned int base, unsigned int exp);
-
 	unsigned int toNum(const Cell (&board)[3][3]);
 
 	std::array<std::array<Cell, 3>, 3> toBoard(unsigned int num);
